beb1e3308305a890ec31d66d2831aaeef14f6251
George W F Santos <gweverton21@hotmail.com>
2021-02-12 20:45:25 -0300

    create app and start routing structure

---
+++ web/src/components/JoinRoom/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const JoinRoom: React.FC = () => {
+  return <div />;
+};
+
+export default JoinRoom;
---
+++ web/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import JoinRoom from "./components/JoinRoom";
+import Chat from "./components/Chat";
+
+// import { Container } from './styles';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={JoinRoom} />
+      <Route path="/chat" component={Chat} />
+    </Switch>
+  );
+};
+
+export default Routes;
