ab8eca52c5933c31afc9f74f4ed9a141e238d54b
George W F Santos <gweverton21@hotmail.com>
2021-02-12 23:26:42 -0300

    create socket connection on the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,21 @@ import express from "express";
 import socketio from "socket.io";
 import http from "http";
 
+import routes from "./routes";
+
 const app = express();
 
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
-server.listen(5000, () => console.log("server is running"));
+io.on("connection", (socket) => {
+  console.log("a new user has connected to the app");
+
+  socket.on("disconnect", () => {
+    console.log("user has desconnected from the app");
+  });
+});
+
+app.use(routes);
+
+server.listen(3333, () => console.log("server is running"));
