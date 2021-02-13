a4df44a494c74684d98876202526d0c14067c6e2
George W F Santos <gweverton21@hotmail.com>
2021-02-12 23:25:35 -0300

    build JoinRoom component elements

--- web/src/components/Chat/index.tsx
+++ web/src/components/Chat/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 // import { Container } from './styles';
+interface JoinRoomRouteParams {
+  roomTitle: string;
+  userName: string;
+}
 
 const Chat: React.FC = () => {
-  return <h1>Chat</h1>;
+  const routeParams = useParams<JoinRoomRouteParams>();
+
+  return <h1>{`Chat: ${routeParams.roomTitle}${routeParams.userName}`}</h1>;
 };
 
 export default Chat;
--- web/src/components/JoinRoom/index.tsx
+++ web/src/components/JoinRoom/index.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
 
-// import { Container } from './styles';
+import { Container, ChatContainer, SubmitButton } from "./styles";
 
 const JoinRoom: React.FC = () => {
-  return <h1>Join Room</h1>;
+  const history = useHistory();
+
+  const [userName, setUserName] = useState("");
+  const [roomTitle, setRoomTitle] = useState("");
+
+  const handleUserNameChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+
+      setUserName(e.target.value);
+    },
+    []
+  );
+
+  const handleRoomTitleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+
+      setRoomTitle(e.target.value);
+    },
+    []
+  );
+
+  const handleSignIn = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      if (!userName || !roomTitle) {
+        alert("error"); // change this to a tosat later;
+
+        return;
+      }
+
+      history.push(`/chat/${userName}/${roomTitle}`);
+    },
+    [history, roomTitle, userName]
+  );
+
+  return (
+    <Container>
+      <ChatContainer>
+        <h1>Join</h1>
+        <form onSubmit={handleSignIn}>
+          <input
+            type="text"
+            name="userName"
+            onChange={handleUserNameChange}
+            placeholder="User name"
+            value={userName}
+          />
+          <input
+            type="Room"
+            name="roomTitle"
+            onChange={handleRoomTitleChange}
+            placeholder=""
+            value={roomTitle}
+          />
+          <SubmitButton type="submit">Sign in</SubmitButton>
+        </form>
+      </ChatContainer>
+    </Container>
+  );
 };
 
 export default JoinRoom;
