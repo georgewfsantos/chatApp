1b1f1c604fedd0622d456965da041afbbf84c1a7
George W F Santos <gweverton21@hotmail.com>
2021-02-15 11:47:37 -0300

    add .env to gitignore file and create .example.env file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,34 @@
 import express from "express";
 import socketio from "socket.io";
 import http from "http";
+// import cors from "cors";
 
 import routes from "./routes";
 
+interface SocketParameters {
+  userName: string;
+  roomTitle: string;
+}
+
 const app = express();
+// app.use(cors());
 
 const server = http.createServer(app);
-const io = new socketio.Server(server);
+const io = new socketio.Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("a new user has connected to the app");
 
+  socket.on("join", ({ userName, roomTitle }: SocketParameters) => {
+    console.log(userName, roomTitle);
+  });
+
   socket.on("disconnect", () => {
-    console.log("user has desconnected from the app");
+    console.log("user has disconnected from the app");
   });
 });
 
